4582f629d792f150633836136117930483195557
Veronika <shuichi4@gmail.com>
2019-11-11 11:41:18 -0500

    Added permission, styles, colors and patients list act

---
+++ app/src/main/java/com/example/supernurse/AdminPatientsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.supernurse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPatientsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_patients_list);
+    }
+}
