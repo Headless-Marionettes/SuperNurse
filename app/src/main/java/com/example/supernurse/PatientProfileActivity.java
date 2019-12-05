729454a51ce4fe233073ffdd3073ce3d8edd489d
Veronika <shuichi4@gmail.com>
2019-12-05 18:04:46 -0500

    Add Android service

--- app/src/main/java/com/example/supernurse/PatientProfileActivity.java
+++ app/src/main/java/com/example/supernurse/PatientProfileActivity.java
@@ -1,9 +1,14 @@
 package com.example.supernurse;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.supernurse.models.Patient;
+import com.example.supernurse.services.LoadInvoicesService;
 import com.example.supernurse.view_models.PatientViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -26,7 +31,7 @@ public class PatientProfileActivity extends AppCompatActivity {
 
         //Get Patient from previous intent
         Intent detailsIntent = getIntent();
-        Patient thePatient = (Patient)detailsIntent.getSerializableExtra("patient");
+        Patient thePatient = (Patient) detailsIntent.getSerializableExtra("patient");
 
         //  Initialise ViewModel here
         patientViewModel =
@@ -47,6 +52,42 @@ public class PatientProfileActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+       // if (thePatient.getInvoices().size() == 0) {
+            startService(new Intent(getBaseContext(), LoadInvoicesService.class));
+     //   }
     }
 
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(LoadInvoicesService.INFO_INTENT)) {
+                String info = intent.getStringExtra("loadPDF");
+
+                patientViewModel.addInvoice(info);
+
+                Log.i("!!!!!!", info);
+
+                Patient p = patientViewModel.getPatient().getValue();
+                int invoicesSize = p.getInvoices().size();
+                Log.i("!!!!!!****", Integer.toString(invoicesSize));
+
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //This needs to be in the activity that will end up receiving the broadcast
+        registerReceiver(receiver, new IntentFilter("loadPDF"));
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
+    }
 }
